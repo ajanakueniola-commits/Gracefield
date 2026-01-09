1dfc11a2421906a862e27a60624f55abf1ddc1af
Funmi123 <ajanakueniola@gmail.com>
2026-01-09 20:19:08 +0100

    nice2

--- terraform/main.tf
+++ terraform/main.tf
@@ -155,31 +155,31 @@ resource "aws_instance" "app" {
 ####################
 # Jenkins Instance (Public)
 ####################
-resource "aws_instance" "jenkins" {
-  ami                    = var.ami_id != "" ? var.ami_id : data.aws_ami.packer_or_amazon.id
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public[0].id
-  vpc_security_group_ids = [aws_security_group.grace.id]
+# resource "aws_instance" "jenkins" {
+#   ami                    = var.ami_id != "" ? var.ami_id : data.aws_ami.packer_or_amazon.id
+#   instance_type          = var.instance_type
+#   subnet_id              = aws_subnet.public[0].id
+#   vpc_security_group_ids = [aws_security_group.grace.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    sudo yum install java-17-amazon-corretto -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum install packer -y
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum install terraform -y
-  EOF
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     yum update -y
+#     sudo yum install java-17-amazon-corretto -y
+# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+# sudo yum install jenkins -y
+# sudo systemctl enable jenkins
+# sudo systemctl start jenkins
+# sudo yum install -y yum-utils shadow-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# sudo yum install packer -y
+# sudo yum install -y yum-utils shadow-utils
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# sudo yum install terraform -y
+#   EOF
 
-  tags = { Name = "jenkins" }
-}
+#   tags = { Name = "jenkins" }
+# }
 
 ####################
 # PostgreSQL Instance (Private)
